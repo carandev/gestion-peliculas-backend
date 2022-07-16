177d11cdbdce29e57f64cfeb3080ddcb8dc72d21
Carlos Gomez <carlosandres0741@gmail.com>
2022-07-16 12:12:11 -0500

    Connect to database

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,20 @@
 import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
 
-export const sequelize = new Sequelize('db_movies', 'root', 'admin', {
-  host: 'localhost',
-  port: '5432',
+dotenv.config();
+
+const {
+  POSTGRESQL_HOSTNAME, 
+  POSTGRESQL_PORT,
+  POSTGRESQL_USER,
+  POSTGRESQL_PASSWORD,
+  POSTGRESQL_DB
+} = process.env;
+
+console.log(POSTGRESQL_DB);
+
+export const sequelize = new Sequelize(POSTGRESQL_DB, POSTGRESQL_USER, POSTGRESQL_PASSWORD, {
+  host: POSTGRESQL_HOSTNAME,
+  port: POSTGRESQL_PORT,
   dialect: 'postgres',
 });
